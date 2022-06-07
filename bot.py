fcb8077da0b7d8e048fa2251f160f30b8fe79894
Wolftyavg <satohola@protonmail.com>
2022-06-07 22:49:26 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,8 +3,8 @@ from discord.ext import commands
 
 bots=commands.Bot(command_prefix='[')
 
-@bot.event
+@bots.event
 async def onready():  #async 定義一個新東西
     print(">>Bot is online<<")
 
-bots.run()
+bots.run('OTgzNzA0NjMwNDY4OTY0NDUy.G33fCz.ZnLuVARocx-VNxWPI-VAoxZPv0OIkCdRaDIBA4') #貼上token
