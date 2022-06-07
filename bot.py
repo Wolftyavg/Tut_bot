aeca6e6ea95af7141f0c3a53c54ebf387c46dcf8
Wolftyavg <satohola@protonmail.com>
2022-06-07 23:21:47 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,4 +7,13 @@ bots=commands.Bot(command_prefix='[')
 async def onready():  #async 定義一個新東西
     print(">>Bot is online<<")
 
-bots.run('OTgzNzA0NjMwNDY4OTY0NDUy.G33fCz.ZnLuVARocx-VNxWPI-VAoxZPv0OIkCdRaDIBA4') #貼上token
+@bots.event
+async def on_member_join(member):  #成員
+    print(f'{member} join!')
+
+@bots.event
+async def on_member_remove(member):  #成員
+    print(f'{member} leave!')
+
+
+bots.run('OTgzNzA0NjMwNDY4OTY0NDUy.GbTITa.mB7c4lcde-9w-RUrP1s3qoeS6LEbk-nXZmeq-8') #貼上token
